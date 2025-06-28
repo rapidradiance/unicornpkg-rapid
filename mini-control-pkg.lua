9918096461a2a231c664c532269e63bd8602c9f4
Rapid Radiance <Rapidpony1@proton.me>
2025-06-28 14:59:54 -0700

    Update mini-control-pkg.lua

--- mini-control-pkg.lua
+++ mini-control-pkg.lua
@@ -5,7 +5,7 @@ package.version = "1.0.0"
 package.desc = "Mini Tardis GUI Controls using computer craft"
 package.instdat = {}
 package.instdat.filemaps = {}
-package.instdat.filemaps["HjziPgu9"] = "/bin/tardis-control-gui.lua"
+package.instdat.filemaps["HjziPgu9"] = "/bin/mini-control-pkg.lua"
 package.pkgType = "com.pastebin"
 package.unicornSpec = "v1.0.0"
 
