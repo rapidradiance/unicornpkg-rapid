fb28eb585a76898e20d20729e74a7366b822aa67
Rapid Radiance <Rapidpony1@proton.me>
2025-07-07 18:18:35 -0700

    Update lock-screen.lua

--- lock-screen.lua
+++ lock-screen.lua
@@ -2,10 +2,12 @@ local package = {}
 package.name = "lock-screen"
 package.maintainer = "Rapid Radiance"
 package.version = "1.0.0"
-package.desc = "Locks the pc with a password, remember to change the password"
+package.desc = "Locks the pc with a password, remember to change the password or it wont work, you can relock the pc by restarting it or typeing lock in the console"
 package.instdat = {}
 package.instdat.filemaps = {}
 package.instdat.filemaps["eAwYQ8TC"] = "/startup/lockscreen.lua"
+package.instdat.filemaps["gMNKymHR"] = "/bin/lock"
+package.dirs = { "/startup" }
 package.pkgType = "com.pastebin"
 package.unicornSpec = "v1.0.0"
 
